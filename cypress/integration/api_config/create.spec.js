8045f9c67c23291aa9b446598dc62ac11fc48ca6
thecla9 <preciouschy4christ@yahoo.com>
2022-05-08 18:24:15 +0100

    Api_config was created by Thecla

---
+++ cypress/integration/api_config/create.spec.js
@@ -0,0 +1,85 @@
+/// <reference types="Cypress" />
+import RandText from '../POM/RandText';
+
+
+const varText = new RandText();
+const user = { 
+    configurl_01:   ("/app-config/create")
+};
+  
+// CONFIG: Create CarListing Config with existing Credentails
+describe('CONFIG: Create CarListing Config (POST)', () => {
+
+//     it('Create Api carlisting Config', () => {
+//         cy.request({
+//               method: 'POST',
+//             url: user.configurl_01,
+//             headers: {
+//                 "authorization": (Cypress.env('token'))
+//           },
+//             body:             {
+//                     "carListingConfig":{
+//                             "minimumCarYearAllowed": varText.cvcNumb   
+//                     },
+//                     "bookingConfig":{
+//                          "checkInFeeFromGuest": varText.cvcNumb,
+//                          "checkInFeeFromHost": varText.cvcNumb
+//                     }
+//             }
+//         }).then((res)=>{
+          
+//               expect(res.status).to.eq(409)
+
+//         })
+//   });
+
+
+    it('Create car listing config with invalid Credentails', () => {
+        cy.request({
+                method: 'POST',
+                failOnStatusCode: false,
+                url: user.configurl_01,
+                headers: {
+                    "authorization": (Cypress.env('token'))  + "SDGH"
+              },
+  
+            body: {
+                
+                    "carListingConfig":{
+                            "minimumCarYearAllowed": 2000   
+                    },
+                    "bookingConfig":{
+                         "checkInFeeFromGuest": 0.1,
+                         "checkInFeeFromHost": 0.03
+                    }
+                }
+        }).then((res)=>{
+              expect(res.status).to.eq(400)
+              expect(res.body.error).to.eq('Bad Request')
+              
+        })
+  });
+
+
+    it('Test carlisting config Creation using invalid checkInFeeFromGuest', () => {
+        cy.request({
+                method: 'POST',
+                failOnStatusCode: false,
+                url: user.configurl_01,
+  
+            body: {
+                "carListingConfig":{
+                        "minimumCarYearAllowed": 2000   
+                },
+                "bookingConfig":{
+                     "checkInFeeFromGuest":"cdftt",
+                     "checkInFeeFromHost": 0.03
+                }
+            }
+        }).then((res)=>{
+              expect(res.status).to.eq(401)
+              expect(res.body.error).to.eq('Unauthorized')
+        })
+  })
+
+})
